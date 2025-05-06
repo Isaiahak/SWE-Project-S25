188935781cfd847b6b66902fdc764106a234b681
panem-enzo <epanem@my.yorku.ca>
2025-05-06 01:12:12 -0400

    Small changes to sidebar

--- frontend/swe-s25-project/src/components/ActionButton.tsx
+++ frontend/swe-s25-project/src/components/ActionButton.tsx
@@ -1,3 +1,7 @@
-export default function ActionButton() {
-    
+export default function ActionButton({label}) {
+    return (
+        <button className="p-4 font-bold text-gray-200 text-shadow-xs hover:bg-sidebar-hover hover:text-secondary hover:cursor-pointer hover:text-shadow-md">
+            {label}
+        </button>
+    );
 }
--- frontend/swe-s25-project/src/components/SideBar.tsx
+++ frontend/swe-s25-project/src/components/SideBar.tsx
@@ -1,7 +1,20 @@
+import ActionButton from "./ActionButton";
+
+import { Goal, GoalIcon } from "lucide-react";
+
 export default function Sidebar() {
   return (
-    <nav className="h-screen w-[20vw] flex flex-col bg-sidebar-primary shadow-md">
-
+    <nav className="h-screen w-80 flex flex-col justify-between bg-sidebar-primary shadow-md">
+      <h1 className="text-center font-[Bebas Neue] font-bold">
+        Aken Studios
+      </h1>
+      <div className="flex flex-col">
+        <ActionButton label="PLAY" />
+        <ActionButton label="CREATE" />
+        <ActionButton label="JOIN" />
+        <ActionButton label="FRIENDS" />
+      </div>
+      <div></div>
     </nav>
   );
 }
