d9a567282a9788e616487afd0dca13a66318930a
Enzo Panem <epanem@my.yorku.ca>
2025-05-05 22:36:31 -0400

    Small changes, simple layout additions

--- frontend/swe-s25-project/src/App.tsx
+++ frontend/swe-s25-project/src/App.tsx
@@ -1,9 +1,15 @@
+import Sidebar from "./Sidebar";
+
 export default function App() {
-  
   return (
     <>
-      <div className={`h-screen w-full flex flex-col items-center text-shadow-md`}>
-        <h1 className={`text-secondary text-xl font-bold`}>SWE S25 Project</h1>
+      <div className={`h-screen w-full flex items-center text-shadow-xs`}>
+        <Sidebar />
+        <div>
+          <h1 className={`text-secondary text-xl font-bold`}>
+            Game of Nodes
+          </h1>
+        </div>
       </div>
     </>
   );
--- frontend/swe-s25-project/src/Sidebar.tsx
+++ frontend/swe-s25-project/src/Sidebar.tsx
@@ -1,3 +1,23 @@
-export default function Sidebar() {
+export default function Sidebar({ children }) {
+  return (
+    <nav className="h-screen flex flex-col bg-sidebar-primary border-r border-gray-300 shadow-sm ">
+      <div className="p-4 pb-2 flex justify-between items-center">
+        <h1>SWE S25 Project</h1>
+      </div>
+      <ul className="flex-1 px-3">{children}</ul>
+      <div className="border-t border-gray-300 flex p-3">
+        <div className="w-10 h-10 rounded-md bg-gray-300"></div>
+        <div className={`flex justify-between items center w-52 ml-3`}></div>
+        <div className="leading-4">
+            <h4 className="font-semibold">Enzo Panem</h4>
+            <span className="text-xs text-gray-500">panemjf@gmail.com</span>
+        </div>
+        <MoreVertical size={20}/>
+      </div>
+    </nav>
+  );
+}
+
+export default function SidebarItem() {
     
 }
