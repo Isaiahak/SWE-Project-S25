6457c3ccd9d31d2fd24f681d9cb02617c334ae20
panem-enzo <epanem@my.yorku.ca>
2025-05-06 14:35:07 -0400

    Setting up basic react router pages

---
+++ frontend/swe-s25-project/src/pages/LobbyPage.tsx
@@ -0,0 +1,7 @@
+export default function LobbyPage() {
+    return (
+        <div className="h-screen w-full bg-primary">
+            <h1>Lobby Page</h1>
+        </div>
+    );
+}
---
+++ frontend/swe-s25-project/src/pages/NotFoundPage.tsx
@@ -0,0 +1,7 @@
+export default function NotFoundPage() {
+    return (
+        <div className="h-screen w-full bg-primary flex justify-center items-center">
+            <h1 className="text-[8rem]">Error 404: Not Found</h1>
+        </div>
+    );
+}
