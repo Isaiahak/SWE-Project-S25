222b97c7e27be584a844ad05c7af35bedd899de1
panem-enzo <epanem@my.yorku.ca>
2025-05-06 00:35:31 -0400

    Small changes

--- frontend/swe-s25-project/src/components/GameBar.tsx
+++ frontend/swe-s25-project/src/components/GameBar.tsx
@@ -1,6 +1,6 @@
 export default function GameBar() {
     return (
-        <div className="mt-8 mb-4 rounded-2xl bg-white w-[70vw] h-36">
+        <div className="mt-8 mb-4 rounded-2xl bg-white w-[70vw] h-36 inset-shadow-md">
 
         </div>
     );
--- frontend/swe-s25-project/src/components/SideBar.tsx
+++ frontend/swe-s25-project/src/components/SideBar.tsx
@@ -1,18 +1,7 @@
 export default function Sidebar() {
   return (
-    <nav className="h-screen flex flex-col bg-sidebar-primary border-r border-gray-300 shadow-sm">
-      <div className="p-4 pb-2 flex justify-between items-center">
-        <h1>SWE S25 Project</h1>
-      </div>
-      <ul className="flex-1 px-3"></ul>
-      <div className="border-t border-gray-300 flex p-3">
-        <div className="w-10 h-10 rounded-md bg-gray-300"></div>
-        <div className={`flex justify-between items center w-52 ml-3`}></div>
-        <div className="leading-4">
-            <h4 className="font-semibold">John Doe</h4>
-            <span className="text-xs text-gray-500">john.doe@gmail.com</span>
-        </div>
-      </div>
+    <nav className="h-screen w-[20vw] flex flex-col bg-sidebar-primary shadow-md">
+
     </nav>
   );
 }
