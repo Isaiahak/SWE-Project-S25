77a47cb1979abb576f6b22688bdec72d4dc486c6
isaiah <isaiahkenny321123@gmail.com>
2025-05-06 23:02:23 -0400

    adding join lobby

--- Backend/test.go
+++ Backend/test.go
@@ -10,9 +10,14 @@ import (
 
 // struct for the lobby objects the server will contain
 type lobby struct {
-	ID          string `json:id`
+	LobbyID          string `json:id`
 	GameID      string `json:game_id`
 	PlayerCount int32  `json:player_count`
+	LobbyType string `json:lobby_type`
+	PreferredLanguage string `json:language`
+	ChatID string `json:chat_id`
+	Difficulty string `json:difficulty`
+
 }
 
 type user struct {
@@ -21,19 +26,27 @@ type user struct {
 }
 
 // list of users
-var users = make(map[string]user)
+var users = make(map[string]*user)
 
 // splice of lobby structs containing initial lobbies
-var lobbies = make(map[string]lobby)
+var lobbies = make(map[string]*lobby)
+
+// map of lobby pairs
+var lobbyMembers = make(map[string][]*user)
+
 
 func getLobbies(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, lobbies)
 }
 
+func joinLobby(c *gin.Context){
+
+}
+
 func createLobby(c *gin.Context) {
 	var input struct {
 		GameID     string `json:"game_id" binding:"required"`
-		UserExists `json:"user_exists" binding:"required"`
+		UserID     string `json:"user_id"`
 	}
 
 	if err := c.ShouldBindJSON(&input); err != nil {
@@ -46,30 +59,34 @@ func createLobby(c *gin.Context) {
 	// creates a instance of a lobby for the user
 	lobbyID := uuid.New().String()
 	newLobby := lobby{
-		ID:          lobbyID,
+		LobbyID:          lobbyID,
 		GameID:      input.GameID,
 		PlayerCount: 1,
 	}
-	lobbies[lobbyID] = newLobby
+	//saving the pointers of the new lobbies
+	lobbies[&lobbyID] = &newLobby
 
 	log.Println("does the current user exists: ", input.UserExists)
 	log.Println("the amount of users is: 4", len(users))
 	log.Println("the amount of lobbies is: ", len(lobbies))
 	log.Println("game id is: ", input.GameID)
 
-	if input.UserExists == false {
+	if input.UserID == "" {
 		userID := uuid.New().String()
 		newUser := user{
 			UserID:   userID,
 			UserName: "new User",
 		}
-		users[userID] = newUser
+		//saving the pointer of the user
+		users[&userID] = &newUser
+		lobbyMembers[lobbyID] = append(userID)
 		c.IndentedJSON(http.StatusCreated, gin.H{
 			"message": "created user and lobby",
 			"user":    newUser,
 			"lobby":   newLobby,
 		})
 	} else {
+		lobbyMembers[lobbyID] = append(input.UserID)
 		c.IndentedJSON(http.StatusCreated, gin.H{
 			"message": "created lobby",
 			"lobby":   newLobby,
